5186f7411985812b86b15ed5de9d78046e5f6a3a
DrDCB <113240568+DrDCB@users.noreply.github.com>
2025-08-06 12:54:06 +1000

    Updated title screen tape hide script

--- models/titlescreen/dcb_title_tape_hide.gd
+++ models/titlescreen/dcb_title_tape_hide.gd
@@ -1,5 +1,9 @@
 extends Spatial
 
 func _ready():
+	# Hide the vanilla tape mesh, rather than editing the scene to delete it.
 	var origtape = $"../cassette_tape"
 	origtape.visible = false
+	# Move the camera back a touch so it isn't right up against the tapes. Vanilla Z position is 46.
+	var cam = $"../../DefaultVirtualCamera"
+	cam.transform.origin.z = 48
